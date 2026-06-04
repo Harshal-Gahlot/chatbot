03157037dbdb2956e84e0533604bfbb02fc15b21
Harshal <gahlotharshal@gmail.com>
2026-06-04 11:59:58 +0530

    made custom generic exception handling function, tho mainly for chat fn only

---
+++ llm_error_handling.py
@@ -0,0 +1,14 @@
+import sys, traceback, config
+RED = "\033[31m"
+RESET = "\033[0m"
+
+def handle_error(fun: function, *args):
+    try:
+        fun(*args)
+    except Exception as e:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        print("\nUnder CODE BLOCK: ",RED,traceback.extract_tb(exc_tb)[1].line, RESET)
+        print("line number: ",traceback.extract_tb(exc_tb)[1].lineno)
+        print("function name: ",traceback.extract_tb(exc_tb)[1].name)
+        print("filename: ", "/".join(traceback.extract_tb(exc_tb)[1].filename.split("/")[-2:]))
+        print("ERROR MESSAGE:",RED, str(e), RESET)
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from openai import OpenAI
+from llm_error_handling import handle_error
 import config
 
-client = OpenAI(
-    api_key=config.API_KEY,
-    base_url=config.BASE_URL
-)
+def chat(prompt: str, messages: list[dict[str, str]]):
+    messages.append({"role": "user", "content": prompt})
 
-messages = [ {"role":"system","content":config.SYS_PROMPT}]
-
-def chat(prompt: str):
     completion = client.chat.completions.create(
         model=config.MODEL,
         temperature=config.TEMPERATURE,
@@ -16,15 +12,19 @@ def chat(prompt: str):
     )
 
     response = completion.choices[0].message.content
-
     print(response)
-    messages.append({"role": "user", "content": prompt})
     messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == "__main__":
+    client = OpenAI(
+        api_key=config.API_KEY,
+        base_url=config.BASE_URL
+    )
+    messages = [ {"role":"system","content":config.SYS_PROMPT}]
+
     while True:
         user_input = input("User: ")
         if "bye" in user_input.lower().strip().split():
             break
-        chat(user_input)
+        handle_error(chat, user_input, messages)
