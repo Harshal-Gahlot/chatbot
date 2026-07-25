aff4b624059d17b4e0e35eb59ee0aaebb814e579
Harshal <gahlotharshal@gmail.com>
2026-07-25 09:47:39 +0530

    updated config.py so both local and prod can use the new prompt

--- config.py
+++ config.py
@@ -4,13 +4,21 @@ load_dotenv()
 from supabase import create_client, Client
 
 class InitEnv:
-    SYS_PROMPT: str = "You are a Chatbot built by a person named Harshal. If you make a mistake or get corrected, admit it immediately and do not invent fake cover stories or facts."
+    SYS_PROMPT: str = "You are a Chatbot built by a person named Harshal. If you make a mistake or get corrected, admit it immediately and do not invent fake cover stories or facts. You are his digiat clone ai, if any recrutor chats with you, you must try to convence them that he's a good candidate unless either the role is not entry level(i won't qualify for mid/senior SWE role) or unless he genuinly doesn't have tech skills for that role. Otherwise if i do have them consider them since im looking for intership, entry-level & junior tech roles. Do not give personal data until asked for that piece of data. below is his personal data that you shall use to provide user with their answer:\n\n\n"
+    if (content := getenv("prompt_md_content")) is not None:
+        SYS_PROMPT += content
+    else:
+        try:
+            with open("prompt.env.md", "r", encoding="utf-8") as f:
+                SYS_PROMPT += f.read()
+        except FileNotFoundError:
+            print("NOT FOUND EITHER prompt.env.md FILE OR prompt_md_content ENVIRONMENT KEY! continuing with default sys prompt")
 
     def __init__(self, provider):
         self.BASE_URL: str = ""
         self.API_KEY: str = ""
         self.MODEL: str = ""
-        self.TEMPERATURE:str = 0.8
+        self.TEMPERATURE:str = 0.7
         if provider == "local":
             self.use_local()
         else:
@@ -40,9 +48,10 @@ class InitEnv:
         # self.MODEL="meta-llama/llama-3.2-3b-instruct:free",
 
     def use_local(self):
-        self.BASE_URL = "http://172.30.0.1:11434/v1"
-        self.API_KEY = "ollama"
-        self.MODEL = "llama3.2:3b"
+        # self.BASE_URL = "http://172.30.0.1:11434/v1"
+        self.BASE_URL = "http://127.0.0.1:8080/v1"
+        self.API_KEY = "llamacpp"
+        self.MODEL = "gemma4:e4b"
         self.TEMPERATURE = 0.1
         self.DATABASE_URL: str = getenv("DATABASE_URL_LOCAL")
         self.supabase_key: str = getenv("SUPABASE_SECRET_KEY_LOCAL")
